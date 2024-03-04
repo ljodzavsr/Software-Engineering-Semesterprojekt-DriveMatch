759333decf3a8a0884cfaed8e3f8a168a0aea8bb
txljv17 <ljodzvis@students.zhaw.ch>
2024-03-04 22:57:23 +0100

    project created and maven mongodb dependency added

---
+++ drivematch/src/main/java/ch/zhaw/drivematch/DrivematchApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.drivematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivematchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivematchApplication.class, args);
+	}
+
+}
---
+++ drivematch/src/test/java/ch/zhaw/drivematch/DrivematchApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.drivematch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivematchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
