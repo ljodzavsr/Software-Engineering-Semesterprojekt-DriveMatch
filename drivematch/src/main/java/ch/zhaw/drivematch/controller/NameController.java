351549fc1178149c06444c14e2b4b20bec3a6773
txljv17 <ljodzvis@students.zhaw.ch>
2024-05-01 12:57:51 +0200

    lombok adaption model class

--- drivematch/src/main/java/ch/zhaw/drivematch/controller/NameController.java
+++ drivematch/src/main/java/ch/zhaw/drivematch/controller/NameController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.opencsv.CSVReader;
-
 import ch.zhaw.drivematch.model.Name;
 
 @RestController
--- drivematch/src/main/java/ch/zhaw/drivematch/model/Name.java
+++ drivematch/src/main/java/ch/zhaw/drivematch/model/Name.java
@@ -1,30 +1,15 @@
 package ch.zhaw.drivematch.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
 public class Name {
     private String name;
     private String geschlecht;
     private int anzahl;
-
-    public Name(String name, String geschlecht, int anzahl) {
-        this.name = name;
-        this.geschlecht = geschlecht;
-        this.anzahl = anzahl;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getGeschlecht() {
-        return geschlecht;
-    }
-
-    public int getAnzahl() {
-        return anzahl;
-    }
-
-    @Override
-    public String toString() {
-        return "Name [name=" + name + ", geschlecht=" + geschlecht + ", anzahl=" + anzahl + "]";
-    }
 }
+
