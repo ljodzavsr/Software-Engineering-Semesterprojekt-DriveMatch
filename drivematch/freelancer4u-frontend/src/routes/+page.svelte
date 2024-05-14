23c58f9466d718d0f7d3bd93083cab64fda60bd2
txljv17 <ljodzvis@students.zhaw.ch>
2024-05-14 11:39:05 +0200

    Change

--- drivematch/freelancer4u-frontend/src/routes/+page.svelte
+++ drivematch/freelancer4u-frontend/src/routes/+page.svelte
@@ -17,7 +17,7 @@
 </script>
 
 {#if $isAuthenticated}
-    <h1>Welcome to DriveMatch!</h1>
+    <h1>Welcome to DriveMatch!!!</h1>
     <!-- Beispiel für die Verwendung von Bildern im Ordner 'static/images' -->
     <img src="/images/vite.svg" alt="Vite Logo" />
 {:else}
